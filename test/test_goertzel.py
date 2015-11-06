c38000e2dd6f5c9e98e1a2b7ef2f9c3c943a391b
Eric Westphal <eric@dormouse.com>
2015-11-06 22:28:32 +0100

    Real unit tests for goertzel function

--- test/test_goertzel.py
+++ test/test_goertzel.py
@@ -1,5 +1,7 @@
+from unittest import TestCase
 from math import sin, pi, sqrt
 from src.goertzel import scanner
+import csv
 
 
 def generate(frequency, sampling_rate, n):
@@ -8,73 +10,123 @@ def generate(frequency, sampling_rate, n):
     return [int(100*(sin(index*step)+1)) for index in range(n)]
 
 
-def generate_and_test1(frequency, target_frequency, sampling_rate, n):
-    # Test 1
-    print("For test frequency %7.1f:" % frequency)
-    test_data = generate(frequency, sampling_rate, n)
+def generate_and_test0(sample_frequency, target_frequency, sampling_rate, n):
+    # Generate sample
+    test_data = generate(sample_frequency, sampling_rate, n)
 
-    # Do the "basic Goertzel" processing.
-    results = []
-    scan = scanner(results, sampling_rate, target_frequency, n, optimized=False)
+    # Do the "basic Goertzel" processing
+    results = {}
+    scan = scanner(results, [target_frequency], sampling_rate, n,
+                   optimized=False)
     next(scan)
     for d in test_data:
         scan.send(d)
 
-    res = results[-1]
-    print("real = %f imag = %f" % (res.real, res.imag))
-    magnitude2 = (res * res.conjugate()).real
-    print("Relative magnitude squared = %f" % magnitude2)
-    magnitude = sqrt(magnitude2)
-    print("Relative magnitude = %f" % magnitude)
+    res = results[target_frequency][-1]
+    return res
+
+
+def generate_and_test1(sample_frequency, target_frequency, sampling_rate, n):
+    # Generate sample
+    test_data = generate(sample_frequency, sampling_rate, n)
 
     # Do the "optimized Goertzel" processing
-    results = []
-    scan = scanner(results, sampling_rate, target_frequency, n)
+    results = {}
+    scan = scanner(results, [target_frequency], sampling_rate, n)
     next(scan)
     for d in test_data:
         scan.send(d)
 
-    magnitude2 = results[-1]
-    print("Relative magnitude squared = %f" % magnitude2)
-    magnitude = sqrt(magnitude2)
-    print("Relative magnitude = %f" % magnitude)
-    print("")
+    magnitude2 = results[target_frequency][-1]
+    return magnitude2
 
 
-def generate_and_test2(frequency, target_frequency, sampling_rate, n):
-    # Test 2
-    print("Freq=%7.1f   " % frequency)
-    test_data = generate(frequency, sampling_rate, n)
+def generate_and_test2(sample_frequency, target_frequency, sampling_rate, n):
+    # Generate sample
+    test_data = generate(sample_frequency, sampling_rate, n)
 
-    results = []
-    scan = scanner(results, sampling_rate, target_frequency, n, optimized=False)
+    results = {}
+    scan = scanner(results, [target_frequency], sampling_rate, n,
+                   optimized=False)
     next(scan)
     for d in test_data:
         scan.send(d)
 
-    res = results[-1]
-    magnitude2 = (res * res.conjugate()).real
-    print("rel mag^2=%16.5f   " % magnitude2)
-    magnitude = abs(res)
-    print("rel mag=%12.5f" % magnitude)
-    print("")
+    return results[target_frequency][-1]
 
 
-if __name__ == '__main__':
-    test_sample_rate = 8000.0  # 8kHz
-    test_target_frequency = 941.0  # 941 Hz
-    test_n = 205  # Block size
+class Test1(TestCase):
 
-    # Test 1
-    generate_and_test1(test_target_frequency - 250, test_target_frequency,
-                       test_sample_rate, test_n)
-    generate_and_test1(test_target_frequency, test_target_frequency,
-                       test_sample_rate, test_n)
-    generate_and_test1(test_target_frequency + 250, test_target_frequency,
-                       test_sample_rate, test_n)
+    def setUp(self):
+        self.test_sample_rate = 8000.0  # 8kHz
+        self.test_target_frequency = 941.0  # 941 Hz
+        self.test_n = 205  # Block size
 
-    # Test 2
-    for freq in range(int(test_target_frequency-300),
-                      int(test_target_frequency+301), 15):
-        generate_and_test2(freq, test_target_frequency,
-                           test_sample_rate, test_n)
+    def test0_below_frequency(self):
+        res = generate_and_test0(self.test_target_frequency - 250,
+                                 self.test_target_frequency,
+                                 self.test_sample_rate, self.test_n)
+        m2 = (res * res.conjugate()).real
+        m = sqrt(m2)
+        self.assertAlmostEqual(res.real, -360.392059, 1)
+        self.assertAlmostEqual(res.imag, -45.871609, 1)
+        self.assertAlmostEqual(m2, 131986.640625, 0)
+        self.assertAlmostEqual(m, 363.299652, 1)
+
+    def test0_at_frequency(self):
+        res = generate_and_test0(self.test_target_frequency,
+                                 self.test_target_frequency,
+                                 self.test_sample_rate, self.test_n)
+        m2 = (res * res.conjugate()).real
+        m = sqrt(m2)
+        self.assertAlmostEqual(res.real, -3727.528076, 1)
+        self.assertAlmostEqual(res.imag, -9286.238281, 1)
+        self.assertAlmostEqual(m2, 100128688.000000, -3)
+        self.assertAlmostEqual(m, 10006.432617, 1)
+
+    def test0_above_frequency(self):
+        res = generate_and_test0(self.test_target_frequency + 250,
+                                 self.test_target_frequency,
+                                 self.test_sample_rate, self.test_n)
+        m2 = (res * res.conjugate()).real
+        m = sqrt(m2)
+        self.assertAlmostEqual(res.real, 424.038116, 1)
+        self.assertAlmostEqual(res.imag, -346.308716, 1)
+        self.assertAlmostEqual(m2, 299738.062500, -1)
+        self.assertAlmostEqual(m, 547.483398, 1)
+
+    def test1_below_frequency(self):
+        m2 = generate_and_test1(self.test_target_frequency - 250,
+                                self.test_target_frequency,
+                                self.test_sample_rate, self.test_n)
+        m = sqrt(m2)
+        self.assertAlmostEqual(m2, 131986.640625, -1)
+        self.assertAlmostEqual(m, 363.299652, 0)
+
+    def test1_at_frequency(self):
+        m2 = generate_and_test1(self.test_target_frequency,
+                                self.test_target_frequency,
+                                self.test_sample_rate, self.test_n)
+        m = sqrt(m2)
+        self.assertAlmostEqual(m2, 100128688.000000, -3)
+        self.assertAlmostEqual(m, 10006.432617, 0)
+
+    def test1_above_frequency(self):
+        m2 = generate_and_test1(self.test_target_frequency + 250,
+                                self.test_target_frequency,
+                                self.test_sample_rate, self.test_n)
+        m = sqrt(m2)
+        self.assertAlmostEqual(m2, 299738.062500, -1)
+        self.assertAlmostEqual(m, 547.483398, 0)
+
+    def test2(self):
+        with open('goertzel_test_results.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            for v in reader:
+                res = generate_and_test2(float(v['freq']),
+                                         self.test_target_frequency,
+                                         self.test_sample_rate, self.test_n)
+                m2 = (res*res.conjugate()).real
+                m = sqrt(m2)
+                self.assertAlmostEqual(m2/float(v['m2']), 1, 2)
+                self.assertAlmostEqual(m/float(v['m']), 1, 2)
